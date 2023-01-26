d93d491baec3768bffc04dc1dfbb5efa4f0c4de0
Jakub Kuczys <me@jacken.men>
2023-01-26 05:18:05 +0100

    Add public Session.IndexedImages property with a read-only collection

--- src/Indexer/Model/Session.cs
+++ src/Indexer/Model/Session.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Indexer.Model
 {
@@ -8,11 +9,11 @@ namespace Indexer.Model
         public string? SessionFilePath { get; set; }
         public Config Config { get; private set; }
         private readonly List<IndexedImage> _indexedImages;
+        public ReadOnlyCollection<IndexedImage> IndexedImages => new(_indexedImages);
 
         public Session(Config config)
         {
             Config = config;
-            _indexedImages = new();
         }
 
         public void ExportPointsToCSV(string filePath)
