ee1bff3f3eb03bc27d1331669986c70070b67135
Jakub Kuczys <me@jacken.men>
2023-01-28 04:04:59 +0100

    Add base and main view model

---
+++ src/Indexer/ViewModel/ViewModelBase.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Indexer.ViewModel
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        ~ViewModelBase()
+        {
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing) { }
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
