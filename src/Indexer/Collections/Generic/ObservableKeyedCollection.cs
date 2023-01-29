828d9e703eb0e876e9df5ce59573fd7338478f04
Jakub Kuczys <me@jacken.men>
2023-01-29 03:37:19 +0100

    Add ObservableKeyedCollection
    Read-only variant of this would also be useful in the future
    but this will do for now.

---
+++ src/Indexer/Collections/Generic/ObservableKeyedCollection.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Indexer.Collections.Generic
+{
+    public abstract class ObservableKeyedCollection<TKey, TItem>
+        : KeyedCollection<TKey, TItem>, INotifyCollectionChanged, INotifyPropertyChanged
+        where TKey : notnull
+    {
+        public event NotifyCollectionChangedEventHandler? CollectionChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private bool _deferCollectionChanged;
+
+        protected override void ClearItems()
+        {
+            base.ClearItems();
+            OnCollectionChanged(
+                new NotifyCollectionChangedEventArgs(
+                    NotifyCollectionChangedAction.Reset
+                )
+            );
+        }
+
+        protected override void InsertItem(int index, TItem item)
+        {
+            base.InsertItem(index, item);
+            OnCollectionChanged(
+                new NotifyCollectionChangedEventArgs(
+                    NotifyCollectionChangedAction.Add, item, index
+                )
+            );
+        }
+
+        protected override void SetItem(int index, TItem item)
+        {
+            base.SetItem(index, item);
+            OnCollectionChanged(
+                new NotifyCollectionChangedEventArgs(
+                    NotifyCollectionChangedAction.Replace, item, index
+                )
+            );
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            var item = this[index];
+            base.RemoveItem(index);
+            OnCollectionChanged(
+                new NotifyCollectionChangedEventArgs(
+                    NotifyCollectionChangedAction.Remove, item
+                )
+            );
+        }
+
+        public void AddRange([NotNull] IEnumerable<TItem> items)
+        {
+            _deferCollectionChanged = true;
+            try
+            {
+                foreach (var item in items)
+                {
+                    try
+                    {
+                        Add(item);
+                    }
+                    catch (ArgumentException)
+                    {
+                        Remove(GetKeyForItem(item));
+                        Add(item);
+                    }
+                }
+                _deferCollectionChanged = false;
+            }
+            finally
+            {
+                OnCollectionChanged(
+                    new NotifyCollectionChangedEventArgs(
+                        NotifyCollectionChangedAction.Reset
+                    )
+                );
+            }
+        }
+
+        public void RemoveRange([NotNull] IEnumerable<TKey> keys)
+        {
+            _deferCollectionChanged = true;
+            try
+            {
+                foreach (var key in keys)
+                {
+                    Remove(key);
+                }
+                _deferCollectionChanged = false;
+            }
+            finally
+            {
+                OnCollectionChanged(
+                    new NotifyCollectionChangedEventArgs(
+                        NotifyCollectionChangedAction.Reset
+                    )
+                );
+            }
+        }
+
+        protected virtual void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            if (_deferCollectionChanged)
+            {
+                return;
+            }
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Count)));
+
+            if (CollectionChanged != null)
+            {
+                CollectionChanged(this, e);
+            }
+        }
+    }
+}
