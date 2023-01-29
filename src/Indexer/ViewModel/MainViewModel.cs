0df03c007918056a7baaa2668cf429c5323ac262
Jakub Kuczys <me@jacken.men>
2023-01-29 03:56:07 +0100

    Handle title bar

--- src/Indexer/ViewModel/MainViewModel.cs
+++ src/Indexer/ViewModel/MainViewModel.cs
@@ -6,9 +6,41 @@ namespace Indexer.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        public string ProgramName { get; private set; } = "Indexer";
         private Session? _session;
+        public string SessionFileTitle => _session?.FilePath ?? "Bez tytułu";
         public bool IsSessionOpen => _session != null;
         public bool IsSessionOnDisk => _session != null && _session.FilePath != null;
+        private bool _isSessionModified;
+        public bool IsSessionModified
+        {
+            get => _isSessionModified;
+            private set
+            {
+                if (_isSessionModified != value)
+                {
+                    _isSessionModified = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(Title));
+                }
+            }
+        }
+        public string Title
+        {
+            get
+            {
+                if (_session is null)
+                {
+                    return ProgramName;
+                }
+                var title = $"{SessionFileTitle} - {ProgramName}";
+                if (IsSessionModified)
+                {
+                    title = $"*{title}";
+                }
+                return title;
+            }
+        }
 
         public MainViewModel() { }
 
