a8af75adc1a981fade1150065e0466b5fa1dd3a2
Jakub Kuczys <me@jacken.men>
2023-01-29 04:11:31 +0100

    Implement session creation/loading/closing and application exit
    Co-authored-by: Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>

--- src/Indexer/View/MainWindow.xaml.cs
+++ src/Indexer/View/MainWindow.xaml.cs
@@ -1,12 +1,213 @@
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.Serialization;
 using System.Windows;
+
+using Indexer.ViewModel;
+
+using Microsoft.Win32;
 
 namespace Indexer.View
 {
     public partial class MainWindow : Window
     {
+        private MainViewModel Data => (MainViewModel)DataContext;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CreateSession_Click(object sender, RoutedEventArgs e)
+        {
+            if (!PromptAboutUnsavedChanges())
+            {
+                return;
+            }
+            var configLocation = PromptForConfigLocation();
+            if (configLocation is null)
+            {
+                return;
+            }
+            try
+            {
+                Data.NewSession(configLocation);
+            }
+            catch (SerializationException ex)
+            {
+                ShowSerializationExceptionDialog(ex);
+            }
+        }
+
+        private void LoadSession_Click(object sender, RoutedEventArgs e)
+        {
+            if (!PromptAboutUnsavedChanges())
+            {
+                return;
+            }
+            var sessionLocation = PromptForSessionLocation(saving: false);
+            if (sessionLocation is null)
+            {
+                return;
+            }
+            try
+            {
+                Data.OpenSession(sessionLocation);
+            }
+            catch (SerializationException ex)
+            {
+                ShowSerializationExceptionDialog(ex);
+            }
+        }
+
+        private void SaveSession_Click(object sender, RoutedEventArgs e)
+        {
+            SaveSession();
+        }
+
+        private void SaveSessionAs_Click(object sender, RoutedEventArgs e)
+        {
+            SaveSession(alwaysPrompt: true);
+        }
+
+        private void CloseSession_Click(object sender, RoutedEventArgs e)
+        {
+            if (!PromptAboutUnsavedChanges())
+            {
+                return;
+            }
+            Data.CloseSession();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (!PromptAboutUnsavedChanges())
+            {
+                e.Cancel = true;
+                return;
+            }
+            Data.CloseSession();
+        }
+
+        private bool PromptAboutUnsavedChanges()
+        {
+            if (!Data.IsSessionModified)
+            {
+                return true;
+            }
+            var sessionFileTitle = Data.SessionFileTitle;
+            switch (
+                MessageBox.Show(
+                    owner: this,
+                    caption: "Zapisać zmiany?",
+                    messageBoxText: $"Czy chcesz zapisać zmiany w pliku {sessionFileTitle}?",
+                    button: MessageBoxButton.YesNoCancel,
+                    icon: MessageBoxImage.Warning,
+                    defaultResult: MessageBoxResult.Yes
+                )
+            )
+            {
+                case MessageBoxResult.Yes:
+                    return SaveSession();
+                case MessageBoxResult.No:
+                    return true;
+                case MessageBoxResult.Cancel:
+                    return false;
+            }
+            throw new UnreachableException();
+        }
+
+        private bool SaveSession(bool alwaysPrompt = false)
+        {
+            string? saveLocation = null;
+            if (alwaysPrompt || !Data.IsSessionOnDisk)
+            {
+                saveLocation = PromptForSessionLocation(saving: true);
+                if (saveLocation is null)
+                {
+                    return false;
+                }
+            }
+            Data.SaveSession(saveLocation);
+            return true;
+        }
+
+        private void ShowSerializationExceptionDialog(SerializationException ex)
+        {
+            MessageBox.Show(
+                owner: this,
+                caption: Data.ProgramName,
+                messageBoxText: (
+                    "Program nie może odczytać tego pliku, ponieważ nie jest to"
+                    + " obsługiwany typ pliku lub jest on uszkodzony.\n\n"
+                    + "Szczegóły o błędzie:\n"
+                    + ex.Message
+                ),
+                button: MessageBoxButton.OK,
+                icon: MessageBoxImage.Error
+            );
+        }
+
+        private string? PromptForSessionLocation(bool saving)
+        {
+            FileDialog fileDialog;
+            string title = "";
+            if (saving)
+            {
+                fileDialog = new SaveFileDialog();
+                title = "Zapisz sesję";
+            }
+            else
+            {
+                fileDialog = new OpenFileDialog();
+                title = "Otwórz sesję";
+            }
+            fileDialog.Title = title;
+            fileDialog.DefaultExt = "ixr";
+            fileDialog.Filter = (
+                "Pliki sesji programu Indexer|*.ixr|Wszystkie pliki|*.*"
+            );
+            if (Data.IsSessionOnDisk)
+            {
+                fileDialog.InitialDirectory = Data.SessionFileDirectory;
+                if (saving)
+                {
+                    fileDialog.FileName = Data.SessionFileName;
+                }
+            }
+            else
+            {
+                fileDialog.RestoreDirectory = true;
+            }
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                return fileDialog.FileName;
+            }
+            return null;
+        }
+
+        private static string? PromptForConfigLocation()
+        {
+            var openFileDialog = new OpenFileDialog()
+            {
+                Title = "Wybierz plik konfiguracyjny",
+                DefaultExt = "xml",
+                Filter = (
+                    "Pliki konfiguracyjne programu Indexer|*.xml|Wszystkie pliki|*.*"
+                ),
+                RestoreDirectory = true
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                return openFileDialog.FileName;
+            }
+            return null;
         }
     }
 }
