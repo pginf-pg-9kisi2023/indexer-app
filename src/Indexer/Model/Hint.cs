6f8a6125c9e63e28a60a50c5ab98d61e1d400c89
Jakub Kuczys <me@jacken.men>
2023-01-29 04:44:28 +0100

    Refactor Image model into a view model

--- src/Indexer/Model/Hint.cs
+++ src/Indexer/Model/Hint.cs
@@ -9,19 +9,14 @@ namespace Indexer.Model
         public string Name { get; private set; }
         [DataMember(Name = "description", Order = 1, IsRequired = true)]
         public string Description { get; private set; }
-        private Image _image;
         [DataMember(Name = "imageRef", Order = 2, IsRequired = true)]
-        public string ImagePath
-        {
-            get => _image.Path;
-            private set => _image = new(value);
-        }
+        public string ImagePath { get; private set; }
 
         public Hint(string name, string description, string imagePath)
         {
             Name = name;
             Description = description;
-            _image = new Image(imagePath);
+            ImagePath = imagePath;
         }
     }
 }
