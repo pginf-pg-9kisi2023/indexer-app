cf5705109e1630e4bc640c0c56f39b391cb665bb
Mikołaj Morozowski <themix240@gmail.com>
2023-06-13 13:33:53 +0000

    Implemented path stripping and more responsive side pannel.

---
+++ src/Indexer/View/PathConverter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Indexer.View
+{
+    class PathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            String? strippedPath = Path.GetFileName(value.ToString());
+            if (strippedPath == null)
+            {
+                return "null";
+            }
+            else return strippedPath;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
