6212f1135a44553bf318a4f377d933b0c7257fa0
Jakub Kuczys <me@jacken.men>
2023-06-16 12:50:12 +0200

    Add some comments explaining how auto-resize works

--- src/Indexer/View/AutoResizeGridView.cs
+++ src/Indexer/View/AutoResizeGridView.cs
@@ -10,9 +10,12 @@ namespace Indexer.View
             {
                 if (double.IsNaN(column.Width))
                 {
+                    // If the value is already NaN, we need to first change it
+                    // to anything else to be able to trigger the property change
                     column.Width = column.ActualWidth;
                 }
 
+                // Trigger resize a column to its content
                 column.Width = double.NaN;
             }
             base.PrepareItem(item);
