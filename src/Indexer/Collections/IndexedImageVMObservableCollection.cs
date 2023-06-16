8ea31af689ff9ebe7e2b76431b7f5ef764f9a87e
Jakub Kuczys <me@jacken.men>
2023-06-16 17:18:05 +0200

    Revert unnecessary changes

--- src/Indexer/Collections/IndexedImageVMObservableCollection.cs
+++ src/Indexer/Collections/IndexedImageVMObservableCollection.cs
@@ -1,7 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 
 using Indexer.Collections.Generic;
-using Indexer.Model;
 using Indexer.ViewModel;
 
 namespace Indexer.Collections
@@ -9,17 +8,6 @@ namespace Indexer.Collections
     public class IndexedImageVMObservableCollection
         : ObservableKeyedCollection<string, IndexedImageViewModel>
     {
-        private ReadOnlyKeyedCollection<string, IndexedImage> indexedImages;
-
-        public IndexedImageVMObservableCollection()
-        {
-        }
-
-        public IndexedImageVMObservableCollection(ReadOnlyKeyedCollection<string, IndexedImage> indexedImages)
-        {
-            this.indexedImages = indexedImages;
-        }
-
         protected override string GetKeyForItem([NotNull] IndexedImageViewModel item)
         {
             return item.ImagePath;
